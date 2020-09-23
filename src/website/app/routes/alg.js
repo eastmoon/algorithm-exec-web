bd30693cad56af01bfe5b77bc9b018f1420301ba
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2020-09-23 20:41:43 +0800

    Update alg.js
    Node.js call sshpass connection to algorithm container.

--- src/website/app/routes/alg.js
+++ src/website/app/routes/alg.js
@@ -1,10 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const { exec, spawn } = require("child_process");
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    console.log(req, res);
-    res.send('respond with a resource');
+    // console.log(req, res);
+    exec(`sshpass -p ${process.env.SSH_PASS} ssh -o StrictHostKeyChecking=no ${process.env.SSH_USER}@alg python /repo/main.py`, (error, stdout, stderr) => {
+        if (error) {
+            // console.log(`error: ${error.message}`);
+            res.send(`error: <br />${error.message}`);
+            return;
+        }
+        if (stderr) {
+            //console.log(`stderr: ${stderr}`);
+            res.send(`stderr: <br />${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        res.send(`stdout: <br />${stdout}`);
+    });
 });
 
 module.exports = router;
